cea08ffdb61859715414ee4e0d85884a7599c6cc
Waneed Parkash <waneed.parkash1@gmail.com>
2020-07-15 17:52:46 +0500

    Fix: vertical margin crash issue

--- app/screens/home/home.styles.js
+++ app/screens/home/home.styles.js
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   sliderContainer: {
-    marginVpaddertical: 25,
+    marginVertical: 25,
   },
   sliderImage: {
     backgroundColor: 'black',
