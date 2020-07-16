5227c5cc277c3f285b76436b291c5dcdcff3868e
Waneed Parkash <waneed.parkash1@gmail.com>
2020-07-16 14:05:24 +0500

    ADD: Auth linking in profile screen

---
+++ app/screens/profile/profile-section/profile.style.js
@@ -0,0 +1,91 @@
+import {StyleSheet} from 'react-native';
+import {width, height, COLOR} from '../../../constants';
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    backgroundColor: COLOR.PROFILE_BG,
+  },
+  contentContainer: {
+    width,
+    height: height * 0.80,
+  },
+  profileImage: {
+    width: width * 0.362,
+    height: width * 0.362,
+    borderRadius: width * 0.362,
+    backgroundColor: COLOR.WHITE,
+    alignSelf: 'center',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-start',
+    top:height * 0.04,
+  },
+  profileEdit: {
+    width: width * 0.1,
+    height: width * 0.1,
+    borderRadius: width * 0.1,
+    backgroundColor: COLOR.PRIMARY_COLOR,
+    alignSelf: 'flex-end',
+    alignItems: 'flex-end',
+    justifyContent: 'flex-end',
+    // padding:
+  },
+  align: {
+    width: width,
+    height: height * 0.634,
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
+    paddingTop: 46,
+    backgroundColor: COLOR.WHITE,
+    justifyContent:'flex-start',
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+  heading:{
+    width:width * 0.32,
+    height:height * 0.036,
+    color: COLOR.PROFILE_BG,
+    fontWeight: '700',
+    fontSize: 24,
+    marginBottom: 38,
+  },
+  details:{
+    width:width * 0.9,
+    height:height * 0.08,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    alignSelf: 'center',
+    borderColor:COLOR.MERCURY,
+    borderWidth:0.5,
+  },
+  contain: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    alignSelf: 'center',
+  },
+  Icon: {
+    width: 39,
+    height: 30,
+    alignSelf:'center',
+    left:26,
+  },
+  textDetail: {
+    fontSize: 16,
+    fontWeight: '300',
+    color: COLOR.PROFILE_ITEM,
+    alignSelf: 'center',
+    left: 60,
+  },
+  button: {
+    width: width * 0.016,
+    height: height * 0.015,
+    alignSelf: 'center',
+    right: 26,
+  },
+});
+
+export default styles;
--- app/screens/profile/profile.js
+++ app/screens/profile/profile.js
@@ -12,7 +12,7 @@ import {
   data,
 } from '../../constants';
 import ProfileDetail from '../../components/profiledetails/profileDetail';
-
+import ProfileSection from './profile-section';
 import styles from './profile.style';
 
 class Profile extends Component {
@@ -26,49 +26,7 @@ class Profile extends Component {
       <View
         style={styles.screen}
       >
-      <View style={styles.contentContainer}>
-        <View style={styles.profileImage}>
-          <View style={styles.profileEdit}/>
-        </View>
-        <View style={styles.align}>
-          <Text style={styles.heading}>User Name</Text>
-          <ProfileDetail 
-            styles={styles} 
-            Icon={profile.personal} 
-            chevron={profile.chevron} 
-            textValue={profile.personalText}
-            //onPress={null}
-          />
-          <ProfileDetail 
-            styles={styles} 
-            Icon={profile.myorder} 
-            chevron={profile.chevron} 
-            textValue={profile.myorderText}
-            //onPress={null}
-          />
-          <ProfileDetail 
-            styles={styles} 
-            Icon={profile.personal} 
-            chevron={profile.chevron} 
-            textValue={profile.mymeasurementText}
-            //onPress={null}
-          />
-          <ProfileDetail 
-            styles={styles} 
-            Icon={profile.mywishlist} 
-            chevron={profile.chevron} 
-            textValue={profile.mywishlistText}
-            //onPress={null}
-          />
-          <ProfileDetail 
-            styles={styles} 
-            Icon={profile.chevron} 
-            chevron={profile.chevron} 
-            textValue={profile.paymentText}
-            //onPress={null}
-          />
-        </View>
-      </View>
+        <ProfileSection/>
       </View>
     );
   }
