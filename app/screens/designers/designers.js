a3591a6505e3c9dd6019cdfe1f870781cf150dd4
Waneed Parkash <waneed.parkash1@gmail.com>
2020-07-20 22:37:43 +0500

    Interim

--- app/screens/designers/designers.js
+++ app/screens/designers/designers.js
@@ -1,13 +1,51 @@
 import React, { Component } from 'react';
-import {} from 'react-native';
+import {
+  ScrollView,
+  View,
+  Image
+} from 'react-native';
 
-import {} from '../../components';
+import {
+  Header,
+  Text,
+  Slider,
+} from '../../components';
+import {images, data} from '../../constants';
 import styles from './designers.styles';
 
 class Designers extends Component {
+  _sliderContent = () => {
+    return [1,2,2,2].map(item => {
+      return (
+        <View style={styles.sliderContentContainer}>
+          <Image
+            source={images.dana} 
+            style={styles.sliderDesigner}
+          />
+          <View style={styles.sliderContent}>
+            <Text style={styles.sliderTitle}>Dana</Text>
+            <Text style={styles.sliderDetails}>Ad Slogan or message Goes here</Text>
+          </View>
+        </View>
+      );
+    });
+  }
   render() {
+    const {designers} = data;
     return (
-      null
+      <View style={styles.screen}>
+        <Header
+          transparent
+        />
+        <Slider
+          sliderContainerStyles={styles.sliderContainer}
+          renderedContent={this._sliderContent}
+          paginationStyle={styles.paginationStyle}
+        />
+        <View style={styles.listContainer}>
+
+        </View>
+      </View>
     );
   }
 }
--- app/screens/designers/designers.styles.js
+++ app/screens/designers/designers.styles.js
@@ -1,8 +1,52 @@
 import { StyleSheet } from 'react-native';
-import {} from '../../constants';
+import { COLOR, width, height } from '../../constants';
 
 const styles = StyleSheet.create({
-
+  screen: {
+    flex: 1,
+    backgroundColor: COLOR.WHITE,
+  },
+  sliderContainer: {
+    width: width,
+    height: height * 0.49,
+    position: 'absolute',
+  },
+  sliderContentContainer: {
+    width: width,
+    aspectRatio: 375/401,
+    padding: 16,
+  },
+  sliderDesigner: {
+    width,
+    height: '100%',
+    position: 'absolute',
+    zIndex: -1,
+  },
+  sliderContent: {
+    marginTop: height * 0.20,
+  },
+  sliderTitle: {
+    fontSize: 24,
+    fontWeight: '700',
+    color: COLOR.BROWN_TEXT,
+  },
+  sliderDetails: {
+    fontSize: 12,
+    fontWeight: '600',
+    width: '40%',
+  },
+  paginationStyle: {
+    // top: height * 0.32
+    bottom: height * 0.13,
+  },
+  listContainer: {
+    flex: 1,
+    backgroundColor: COLOR.WHITE,
+    borderTopLeftRadius: 28,
+    borderTopRightRadius: 28,
+    padding: 23,
+    marginTop: height * 0.38
+  },
 });
 
 export default styles;
